ee0610b4eaf146c431e33784d0a24ebd7750f865
dciriya <mintaesshi@nate.com>
2016-11-20 02:17:03 +0900

    report 07

---
+++ Algorithm07.java
@@ -0,0 +1,13 @@
+package 청학동과제;
+public class report1 {
+	public static void main(String[] args) {
+
+	int p = 0, m = 0, h = 0, q = 0, s = 0, H = 0;
+	
+	p++;
+	q = 78-p;
+	m=p*q;
+	s=s+m;
+	System.out.println(H);
+	}
+}
