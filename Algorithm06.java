af86bac517081dde5cee750f86c94e0ddb651dac
dciriya <mintaesshi@nate.com>
2016-11-20 02:16:51 +0900

    report 06

---
+++ Algorithm06.java
@@ -0,0 +1,16 @@
+package 청학동과제;
+public class report2 {
+
+	public static void main(String[] args) {
+	int s = 0, t = 0 ,i = 0;
+	int array[];
+	for (i=0; i<4; i++){
+	array = new int[2];
+	t++;
+	}
+	for(s=2; s>0; s++) {
+		s = 0;
+		break;
+		}
+	}
+}
